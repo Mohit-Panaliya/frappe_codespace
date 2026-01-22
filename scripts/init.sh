f1c4e57f1d45e60684e8af637a9521ce83b07f4a
Mohit-Panaliya <79244051+Mohit-Panaliya@users.noreply.github.com>
2026-01-22 11:11:46 +0530

    Update init.sh

--- scripts/init.sh
+++ scripts/init.sh
@@ -96,7 +96,8 @@ sed -i '/redis/d' Procfile
 bench new-site dev.localhost \
     --mariadb-root-password 123 \
     --admin-password admin \
-    --mariadb-user-host-login-scope='%'
+    --mariadb-user-host-login-scope='%' \
+    --force
 
 bench --site dev.localhost set-config developer_mode 1
 bench --site dev.localhost clear-cache
